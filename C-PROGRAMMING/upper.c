374fbc1a4e9f8b904becb2c2aa088454b284a131
Rucha Pathak <pathakrucha848@gmail.com>
2025-10-31 17:16:30 +0530

    Add files via upload

---
+++ C-PROGRAMMING/upper.c
@@ -0,0 +1,8 @@
+void main()
+{
+	char c;
+	printf("Enter the character: ");
+	scanf("%c",&c);
+	
+	c>='A'&&c<='Z'?printf("Uppercase letter"):printf("Lowercase letter");
+}n
---
+++ C-PROGRAMMING/vowcons.c
@@ -0,0 +1,10 @@
+void main()
+{
+	char c;
+	printf("enter the character: ");
+	scanf("%c",&c);
+	if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+		printf("%c is vowel",c);
+	else
+		printf("%c is consonant",c);
+}
