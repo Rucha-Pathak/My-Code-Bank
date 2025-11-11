fa1323e71783d4b1b061b483d750749580a6e542
Rucha Pathak <pathakrucha848@gmail.com>
2025-11-11 11:50:10 +0530

    Add files via upload

---
+++ JAVA/Treesetdemo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Treesetdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		TreeSet t1 = new TreeSet();
+		
+		   
+		   t1.add(10);
+		   t1.add(20);
+		   t1.add(30);
+		   t1.add(5);
+		   t1.add(20);
+		   System.out.println(t1);
+	}
+
+}
