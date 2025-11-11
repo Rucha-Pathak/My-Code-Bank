0ebbf0b9a845f3323fa0680fbc55ba83baf39028
Rucha Pathak <pathakrucha848@gmail.com>
2025-11-11 11:51:20 +0530

    Add files via upload

---
+++ JAVA/Arraylistdemo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Arraylistdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ArrayList a1 = new ArrayList();
+	LinkedList a1 = new LinkedList();
+		
+		   
+		   a1.add(10);
+		   a1.add(20);
+		   a1.add(30);
+		   a1.add(5);
+		   a1.add(20);
+		   System.out.println(a1);
+
+
+	}
+
+}
